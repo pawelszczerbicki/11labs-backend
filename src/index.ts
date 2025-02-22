afeaacce2d042d05774b7e53d6355d4a330d1234
Pawel Szczerbicki <pawel.szczerbicki@arcsoftware.it>
2025-02-22 13:23:04 +0100

    do not connect db

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
 import 'express-async-errors'
-import {mongo} from "./context";
 import {app} from "./app";
 import {config} from "./utils/config";
 import {logger} from "./utils/logger";
-import admin from 'firebase-admin';
 
-admin.initializeApp()
-mongo.connect(config.DB_URL)
-  .then(() => console.log("Db connected"))
-  .then(() => app.listen(config.PORT, () => logger.info("Server started")))
-  .catch(e => {
-    logger.error("Can not start app", e)
-    throw e
-  })
+// admin.initializeApp()
+app.listen(config.PORT, () => logger.info("Server started"))
