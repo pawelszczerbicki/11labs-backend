3d9fccae8b48a56a1b4db7a208916b5666213fb4
Pawel Szczerbicki <pawel.szczerbicki@arcsoftware.it>
2025-02-22 15:46:46 +0100

    url

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -2,32 +2,33 @@ import {Controller} from "../controller";
 import {Router} from "express";
 import {HelloService} from "./hello.service";
 import {ElevenLabsClient} from "elevenlabs";
-import stream from "stream";
-import {Storage} from "@google-cloud/storage";
+import * as stream from "stream";
 import {config} from "../utils/config";
 
-const bucket = "elevenlabs-hackaton-stories"
 
 export class HelloController implements Controller {
   router = Router();
 
   constructor(private service: HelloService) {
     this.router.get("/", (_, res) => this.get().then(u => res.json(u)));
-    this.router.get("/story", (_, res) => this.get().then(u => res.json(u)));
+    this.router.get("/story", this.getStory);
   }
 
   get = () => this.service.sayHello()
 
-  getStory = async () => {
+  getStory = async (_, res) => {
     const client = new ElevenLabsClient({apiKey: config.ELEVEN_LABS_KEY});
 
-    const audio = await client.textToSpeech.convert("JBFqnCBsd6RMkjVDRZzb", {
+    const audio: stream.Readable = await client.textToSpeech.convert("JBFqnCBsd6RMkjVDRZzb", {
       text: "The first move is what sets everything in motion.",
       model_id: "eleven_multilingual_v2",
       output_format: "mp3_44100_128",
     });
+    res.setHeader('Content-Type', 'audio/mpeg');
+    res.setHeader('Content-Disposition', 'inline; filename="audio.mp3"');
 
-    new Storage()
+    // Przekaż strumień audio do odpowiedzi
+    audio.pipe(res);
   }
 }
 
